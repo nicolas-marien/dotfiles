ee0cdfe3c267f8adb3744aa3c6cd71fdf9b7704c
Nicolas Marien <mariennicolas@icloud.com>
2023-03-16 16:56:43 +0100

    feat: update colors and add neogen

--- dot_config/nvim/lua/plugins/biscuits.lua
+++ dot_config/nvim/lua/plugins/biscuits.lua
@@ -1,10 +1,14 @@
 return {
-	'code-biscuits/nvim-biscuits',
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter', },
-	config = function()
-		require 'nvim-biscuits'.setup {
-			cursor_line_only = true
-		}
-	end
+    'code-biscuits/nvim-biscuits',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+        require 'nvim-biscuits'.setup {
+            cursor_line_only = true,
+            default_config = {
+                max_length = 12,
+                min_distance = 5,
+                prefix_string = " "
+            }
+        }
+    end
 }
--- dot_config/nvim/lua/plugins/colors.lua
+++ dot_config/nvim/lua/plugins/colors.lua
@@ -9,35 +9,27 @@ return {
             vim.cmd([[colorscheme catppuccin-mocha ]])
             vim.g.nicolas_colorscheme = "catppuccin"
         end
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        lazy = false,
-        config = function()
-            vim.cmd([[colorscheme kanagawa]])
+    }, {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    config = function()
+        vim.cmd([[colorscheme kanagawa]])
 
-            vim.g.nicolas_colorscheme = "kanagawa"
+        vim.g.nicolas_colorscheme = "kanagawa"
 
-            require('kanagawa').setup({
-                colors = {
-                    theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none"
-                            }
-                        }
-                    }
-                },
-                overrides = function(colors) -- add/modify highlights
-                    local theme = colors.theme
-                    return {
-                        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-                        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-                        PmenuSbar = { bg = theme.ui.bg_m1 },
-                        PmenuThumb = { bg = theme.ui.bg_p2 },
-                    }
-                end,
-            })
-        end
-    }
+        require('kanagawa').setup({
+            colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+            overrides = function(colors)     -- add/modify highlights
+                local theme = colors.theme
+                return {
+                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
+                    BiscuitColor = { fg = colors.palette.fujiGray }
+                }
+            end
+        })
+    end
+}
 }
