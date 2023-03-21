d29d4a7905442b5a1cc77f327acfd5e6500a93c9
Nicolas Marien <mariennicolas@icloud.com>
2023-03-21 12:26:07 +0100

    feat: add togglterm

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,51 +1,59 @@
 return {
-    'tpope/vim-fugitive',
-    'tpope/vim-rhubarb',
-    {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup {
-                current_line_blame = true,
-                signs = {
-                    add = { text = '·' },
-                    change = { text = '·' },
-                    delete = { text = '·' },
-                    topdelete = { text = '·' },
-                    changedelete = { text = '·' },
-                },
-                on_attach = function(bufnr)
-                    local gs = package.loaded.gitsigns
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+				signs = {
+					add = { text = "" },
+					change = { text = "" },
+					delete = { text = "" },
+					topdelete = { text = "·" },
+					changedelete = { text = "·" },
+				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
 
-                    local function map(mode, l, r, opts)
-                        opts = opts or {}
-                        opts.buffer = bufnr
-                        vim.keymap.set(mode, l, r, opts)
-                    end
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
 
-                    -- Navigation
-                    map('n', ']c', function()
-                        if vim.wo.diff then return ']c' end
-                        vim.schedule(function() gs.next_hunk() end)
-                        return '<Ignore>'
-                    end, { expr = true })
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							return "]c"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
 
-                    map('n', '[c', function()
-                        if vim.wo.diff then return '[c' end
-                        vim.schedule(function() gs.prev_hunk() end)
-                        return '<Ignore>'
-                    end, { expr = true })
-                end
-            }
-        end
-    },
-    {
-        'sindrets/diffview.nvim', dependencies = 'nvim-lua/plenary.nvim'
-    },
-    {
-        'akinsho/git-conflict.nvim',
-        config = function()
-            require('git-conflict').setup()
-        end
-    }
-
+					map("n", "[c", function()
+						if vim.wo.diff then
+							return "[c"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+				end,
+			})
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
+	{
+		"akinsho/git-conflict.nvim",
+		config = function()
+			require("git-conflict").setup()
+		end,
+	},
 }
