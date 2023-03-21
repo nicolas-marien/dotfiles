57695c5aaf3a284b9dd82d62af6862c778ef4560
Nicolas Marien <mariennicolas@icloud.com>
2023-03-21 17:44:09 +0100

    style: format lsp file

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,82 +1,77 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-local mason_lspconfig = require 'mason-lspconfig'
+local mason_lspconfig = require("mason-lspconfig")
 
 local on_attach = function(_, bufnr)
-    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself
-    -- many times.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local nmap = function(keys, func, desc)
-        if desc then desc = 'LSP: ' .. desc end
+	-- NOTE: Remember that lua is a real programming language, and as such it is possible
+	-- to define small helper and utility functions so you don't have to repeat yourself
+	-- many times.
+	--
+	-- In this case, we create a function that lets us more easily define mappings specific
+	-- for LSP related items. It sets the mode, buffer and description for us each time.
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
 
-        vim.keymap.set('n', keys, func, {buffer = bufnr, desc = desc})
-    end
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references,
-         '[G]oto [R]eferences')
-    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols,
-         '[D]ocument [S]ymbols')
-    nmap('<leader>ws',
-         require('telescope.builtin').lsp_dynamic_workspace_symbols,
-         '[W]orkspace [S]ymbols')
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-    -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+	-- See `:help K` for why this keymap
+	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-    -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder,
-         '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder,
-         '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
+	-- Lesser used LSP functionality
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
 
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format',
-                                         function(_) vim.lsp.buf.format() end, {
-        desc = 'Format current buffer with LSP'
-    })
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, {
+		desc = "Format current buffer with LSP",
+	})
 
-    vim.api.nvim_buf_create_user_command(bufnr, "OrganizeImports", function(_)
-        vim.lsp.buf.execute_command({
-            command = "_typescript.organizeImports",
-            arguments = {vim.api.nvim_buf_get_name(0)},
-            title = ''
-        })
-    end, {desc = "Organize imports"})
-
+	vim.api.nvim_buf_create_user_command(bufnr, "OrganizeImports", function(_)
+		vim.lsp.buf.execute_command({
+			command = "_typescript.organizeImports",
+			arguments = { vim.api.nvim_buf_get_name(0) },
+			title = "",
+		})
+	end, { desc = "Organize imports" })
 end
 
-mason_lspconfig.setup_handlers {
-    function(server_name)
-        if server_name ~= 'tsserver' then
-            require('lspconfig')[server_name].setup {
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {}
-            }
-        else
-            require('lspconfig').tsserver.setup {
+mason_lspconfig.setup_handlers({
+	function(server_name)
+		if server_name ~= "tsserver" then
+			require("lspconfig")[server_name].setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {},
+			})
+		else
+			require("lspconfig").tsserver.setup({
 
-                root_dir = require("lspconfig").util
-                    .root_pattern("package.json"),
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {}
-            }
-        end
-    end
-}
+				root_dir = require("lspconfig").util.root_pattern("package.json"),
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {},
+			})
+		end
+	end,
+})
