2bb731fe7a92d39452334209c3a08835c39a7cf0
Nicolas Marien <mariennicolas@icloud.com>
2024-01-30 10:02:35 +0100

    fix: specify stow destination dir

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if command -v stow >/dev/null 2>&1; then
-	stow neovim
-	stow wezterm
-	stow zsh
-	stow tmux
-	stow git
-	stow alacritty
-	stow prompt
+	stow -t ~ neovim
+	stow -t ~ wezterm
+	stow -t ~ zsh
+	stow -t ~ tmux
+	stow -t ~ git
+	stow -t ~ alacritty
+	stow -t ~ prompt
 else
 	echo "stow is not installed"
 	process exit 1
