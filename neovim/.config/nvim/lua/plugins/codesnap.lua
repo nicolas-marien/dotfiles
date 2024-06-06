eaf133e5ba3cdae5a0dfbc5bf465a26176a6da91
Nicolas Marien <mariennicolas@icloud.com>
2024-06-06 15:36:01 +0200

    chore: add codesnap

---
+++ neovim/.config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,12 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make",
+  opts = {
+    border = "rounded",
+    has_breadcrumbs = true,
+    bg_color = "#585b70",
+    watermark = "",
+    has_line_number = true,
+    save_path = "~/Desktop/codesnap.png",
+  },
+}
